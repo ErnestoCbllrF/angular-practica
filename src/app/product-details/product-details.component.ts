db554880a295c78e856527a55ac82dcae4572292
ErnestoCbllrF <102733605+ErnestoCbllrF@users.noreply.github.com>
2022-12-14 13:51:07 +0100

    Commit 2a Parte

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -17,8 +17,7 @@ export class ProductDetailsComponent implements OnInit {
 
     // Find the product that correspond with the id provided in route.
     this.product = products.find(
-      (product) => product.id === productIdFromRoute
-    );
+      (product) => product.id === productIdFromRoute);
   }
   /* ... */
   constructor(private route: ActivatedRoute) {}
